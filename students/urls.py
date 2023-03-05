4d1328b0c236e69fa774497e74e9c90682205a97
nandotoding <nandotodingbua@gmail.com>
2023-03-05 13:16:50 +0700

    added features

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('students/', views.students, name='students'),
+    path('students/<int:id>', views.student, name='student'),
 ]
--- students/views.py
+++ students/views.py
@@ -6,12 +6,14 @@ from .models import Student
 
 @csrf_exempt
 def students(request):
+    response = {}
+
     if request.method == 'GET':
         students = Student.objects.all().values()
-        data = {
-            'data': list(students)
+        response = {
+            'message': 'successfully get all students',
+            'data': list(students),
         }
-        return JsonResponse(data, safe=False)
     elif request.method == 'POST':
         payload = json.loads(request.body)
         student = Student(
@@ -20,6 +22,36 @@ def students(request):
             studentAge=payload['studentAge']
         )
         student.save()
-        return JsonResponse({
-            'message': 'successfully add student data',
-        })
+        response = {
+            'message': 'successfully add student',
+            'data': dict(Student.objects.all().values().get(id=student.id)),
+        }
+    else:
+        response = {
+            'message': 'HTTP request method not supported',
+        }
+
+    return JsonResponse(response, safe=False)
+
+
+@csrf_exempt
+def student(request, id):
+    response = {}
+    students = Student.objects.all()
+
+    if request.method == 'GET':
+        response = {
+            'message': 'successfully get student',
+            'data': dict(students.values().get(id=id)),
+        }
+    elif request.method == 'DELETE':
+        students.get(id=id).delete()
+        response = {
+            'message': 'successfully delete student {}'.format(id),
+        }
+    else:
+        response = {
+            'message': 'HTTP request method not supported',
+        }
+
+    return JsonResponse(response, safe=False)
